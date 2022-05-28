2abd1650cf57062cfe710ec4246a030a5c1c5d3f
Ivo Carbajo <ivocarbajo@gmail.com>
2022-05-29 01:05:41 +0200

    improve importReadingsFrom function

--- indoors-temp-sensor-backend/src/main/java/com/codingneko/indoors_temp_sensor/indoors_temp_sensor_backend/service/ReadingService.java
+++ indoors-temp-sensor-backend/src/main/java/com/codingneko/indoors_temp_sensor/indoors_temp_sensor_backend/service/ReadingService.java
@@ -2,6 +2,7 @@ package com.codingneko.indoors_temp_sensor.indoors_temp_sensor_backend.service;
 
 import com.codingneko.indoors_temp_sensor.indoors_temp_sensor_backend.model.Reading;
 import com.codingneko.indoors_temp_sensor.indoors_temp_sensor_backend.repository.ReadingRepository;
+import com.codingneko.indoors_temp_sensor.indoors_temp_sensor_backend.utils.InvalidImportUrlException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,14 +36,14 @@ public class ReadingService {
         readingRepository.save(reading);
     }
 
-    public void importReadingsFrom(String url) {
+    public void importReadingsFrom(String url) throws InvalidImportUrlException, UnknownHostException {
         List<Reading> readings = new ArrayList<>();
         ResponseEntity<Reading[]> response = restTemplate.getForEntity(url, Reading[].class);
 
-        try {
-            readings = List.of(Objects.requireNonNull(response.getBody()));
-        } catch (NullPointerException e) {
-            System.out.println("Data import function was run, but no readings could be imported because there were none or the request failed");
+        if (response.getBody() != null) {
+            readings = List.of(response.getBody());
+        } else {
+            throw new InvalidImportUrlException();
         }
 
         readingRepository.saveAll(readings);
---
+++ indoors-temp-sensor-backend/src/main/java/com/codingneko/indoors_temp_sensor/indoors_temp_sensor_backend/utils/InvalidImportUrlException.java
@@ -0,0 +1,4 @@
+package com.codingneko.indoors_temp_sensor.indoors_temp_sensor_backend.utils;
+
+public class InvalidImportUrlException extends Exception {
+}
